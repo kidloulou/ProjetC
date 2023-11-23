1261f34dfdb1f0350a34eda91a8395cbee932771
kidloulou <louisdavin@gmail.com>
2023-11-23 09:39:38 +0100

    Initial Commit

---
+++ projet/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  projet
+//
+//  Created by Louis Dv on 23/11/2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
