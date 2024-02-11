68fa8170a6814ef5d0b7a6563e61cbd64e34b215
kidloulou <140658368+kidloulou@users.noreply.github.com>
2024-02-11 01:08:50 +0100

    new projet

---
+++ exo c/main.c
@@ -0,0 +1,14 @@
+//implémenter 3 stuctures et les instancier
+//utiliser 4 allias pour des variable et une structure
+//utiliser les 4 principes de la PDO
+#include <stdio.h>
+
+
+int main(void) {
+   
+ 
+    return 0;
+}
+
+
+
